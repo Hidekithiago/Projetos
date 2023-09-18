2bd15e8b3b7a7ef7f817dc610a402f7dbfc1f68e
Hidekithiago <39059176+Hidekithiago@users.noreply.github.com>
2023-09-18 18:46:26 -0300

    Update DataHora_capturarDiaSemana.py

--- Python/Funcoes/DataHora_capturarDiaSemana.py
+++ Python/Funcoes/DataHora_capturarDiaSemana.py
@@ -1,16 +1,34 @@
 from datetime import datetime
-DIAS = [
-    'Domingo',
-    'Segunda-feira',
-    'Terça-feira',
-    'Quarta-feira',
-    'Quinta-Feira',
-    'Sexta-feira',
-    'Sábado'
-]
-#data = datetime.strptime('2023-05-29', "%'%'%Y-%'%'%m-%'%'%d") #PowerAutomate
-data = datetime.strptime('2023-05-29', "%Y-%m-%d")
+def VerificaDiaSemana():
+    DIAS = [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-Feira',
+        'Sexta-feira',
+        'Sábado'
+    ]
+    #data = datetime.strptime('2023-05-29', "%'%'%Y-%'%'%m-%'%'%d") #PowerAutomate
+    data = datetime.strptime('2023-05-29', "%Y-%m-%d")
+    print(data)
+    print(DIAS[data.isoweekday()])
+    print(data.isoweekday())
 
-print(data)
-print(DIAS[data.isoweekday()])
-print(data.isoweekday())
+def VerificaDiaSemanaAtual():
+    DIAS = [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-Feira',
+        'Sexta-feira',
+        'Sábado'
+    ]   
+
+    #data = datetime.strptime('2023-05-29', "%'%'%Y-%'%'%m-%'%'%d") #PowerAutomate
+    data = datetime.now()
+
+    print(data)
+    print(DIAS[data.isoweekday()])
+    print(data.isoweekday())
